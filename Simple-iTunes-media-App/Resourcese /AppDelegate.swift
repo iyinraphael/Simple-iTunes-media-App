84f90b1891ae37f660cf5a8a4b4fc7c90cf026b8
iyinraphael <iyinraphael@gmail.com>
2019-06-02 20:21:25 -0700

    initial commit

---
+++ Simple-iTunes-media-App/Resourcese /AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Simple-iTunes-media-App
+//
+//  Created by Iyin Raphael on 6/2/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+

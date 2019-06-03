0c6dd43930e3354b9fc6b0563f0fef8aad0d06d7
iyinraphael <iyinraphael@gmail.com>
2019-06-02 21:30:28 -0700

    struct Type created to parse JSON

---
+++ Simple-iTunes-media-App/Model/Media.swift
@@ -0,0 +1,33 @@
+//
+//  Media.swift
+//  Simple-iTunes-media-App
+//
+//  Created by Iyin Raphael on 6/2/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import Foundation
+
+struct Media: Codable {
+    
+    let name: String
+    let artist: String
+    let image: String
+    let mediaType: String
+    
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case name = "name"
+        case artist = "artistName"
+        case image = "artWorkUrl100"
+        case mediaType = "kind"
+    }
+}
+
+
+struct Results: Decodable {
+    
+    var results: [Media]
+
+}
